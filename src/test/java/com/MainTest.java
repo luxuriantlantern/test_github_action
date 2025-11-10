272d5f28041fad40e7e86c8529c0dbc06f295f84
luxuriantlantern <sjtu_chenlingxi@sjtu.edu.cn>
2025-11-10 14:09:57 +0800

    update

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -10,7 +10,31 @@ class MainTest {
 	void test() {
 		Main main = new Main();
 		int result = main.add(1, 1);
-		assertEquals(1, result);
+		assertEquals(2, result);
+	}
+
+	@Test
+	void testAddWithNegativeA() {
+		Main main = new Main();
+		assertThrows(IllegalArgumentException.class, () -> {
+			main.add(-1, 1);
+		});
+	}
+
+	@Test
+	void testAddWithNegativeB() {
+		Main main = new Main();
+		assertThrows(IllegalArgumentException.class, () -> {
+			main.add(1, -1);
+		});
+	}
+
+	@Test
+	void testAddOverflow() {
+		Main main = new Main();
+		assertThrows(ArithmeticException.class, () -> {
+			main.add(Integer.MAX_VALUE, 1);
+		});
 	}
 
 }
